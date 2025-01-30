192c667a0bef0e0dfbae1ac433ca1d790084285b
panku-chavan <pankajchavan963@gmail.com>
2025-01-30 13:03:03 +0530

    home and process page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
-import Home from './Components/Home'
 import Footer from './Components/Footer'
+import Navbar from './Components/Navbar'
 import AppRouter from './routes/AppRouter'
 
 
